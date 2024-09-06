6e6ba5c22cf367e1ce8ffd2a6464d825bfe53d5d
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-09-06 20:37:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(cors({
   credentials: true // If your frontend requires cookies or other credentials
 }));
 
+app.set('view engine', 'ejs');
+
 const GOOGLE_CLIENT_EMAIL ='ogcs-895@tidal-glider-434811-p6.iam.gserviceaccount.com';
 const GOOGLE_PRIVATE_KEY ="-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDrCzBtRfnpEjRf\n+9yM/KQcAZNEATEpmyQR6N5sxM4iaCigQNOkCtLTrsG+ss/KeFL+LF0bCtqgfgrQ\n/2e6NGMr967IFX/8kezO7cTIvmOVEeMpinYhHap2SMac+lYpYCjCdH0WyFJGSMl2\nyw1TW2cDcAIpfn5t8Jah2yQeLLJC4qoBfc9oH/sN1jUAm3a5nyu7VwydK87aHaGQ\nqiikUvgISxXp/O8kJMu9NKJgd3UJV4N9PrlOMvjEEdF3Y4u1/yQ0DMcqT4EjEDw9\nsHPbadrUYqZeNBj39benpev6XOtq18Vhp7hO5FrxB6GNIaTPr+zZlaDJ4pRzPEnY\nIW2izKDxAgMBAAECggEADuXHSJ/LMKWDnhNSxflj5fYIGWL5dJ2FlmCEC5EYTyg6\nrfiZ6X95oCrnExEWmErS2NMjRhnkpFBE230avBvCruhcosgxE+dEYxPBOaeOnIXV\nF/6TkPMGSUfA1fWMhUQX5UZYoNtq0pNduJqzYiWMlJRSgMKInSuHAqSHCCYVWOWR\nndcScMPZEC7ZzxZ9d2fep7s5aLUpp139+kLb22mIO6CjlrRiEbPehQbhSJGd0mDR\npAfaVt5tu9+LHpFnzxgxcVTvHSUnFx1Tm48iuJxpec73hVqMrWN41hbrPvPqkZOR\nkP3D5wppK5ilPmjjy0K36jfA9x7tEd7GkTqpHXuyuQKBgQD7apLAED2sFGyUwJSI\nh8XoV7bkH6xp8GT4nIAM2FzeSymvyKB6EhzEVVtkVSUHzj57ZnBmddkTdJQ4rLmQ\nhCQ8TCl4xuYcaBOVbv5Ln0Eg2jlExDhY7D4RvcA2qVw295C0Gnt6DMAOyaInuLJi\nNX4adzBbPT50ROhDPpemgseEGQKBgQDvVDNfSVJeYTGbA/F66n/3atAW3lP+Hw9j\nuIplnJ8cFBzRPmnSWUFiYmx4hpoZuT58TtPJKs9W2fZIXctiVsNecrMrYMiidW2W\nvKeGNkfb+agf4Jc5LJByHkQyK2/9kOulrW0ReT355NUyRmD3DRIYVmIGXTN2ps6z\n2sQwVSnemQKBgQDfvtSjANEh13taVpjZeJt0TruBAX8bOMljR0PeKp9bZWDQA6ht\nerkHMT+IZw3xiGaqw1u8k5yAZ/uRBIaQSklFMDKoPbqEBUuIbaL6AdygNBVLUaUj\n3frPJyNsggSDFoc9AWRqFfbGMkORPhnitOBpBTGPwU1XhAt/7LBhi3mr4QKBgQDg\nAYKX9EvpVLWWbyltr+GYldwxG2WoXDAOWWMIWoE+ScDzRKTNooclbBQ0919zJkTL\nGwj1qGEq3JW9BDViVO1GELuMpWkFTj4FsYxnfZTi2xk2xQMmG4UTmyqffrKythsq\nFloY2c9df4bhKKiHdC90oh3b8Q5DRzv/cYrh5kRrOQKBgAlcIoq5FjmokKU53+vn\nLxY3vqq8NINyNRyOZ2WKqmvpkuJbKAKbAobf0I6gObtd1k18fBLRa1zLMX7WzkD/\nTK34yXIhy4l+3nLvVknM+oa4BLT5jhP277GmWGma/MLJB4E6RllyIhww74cZrKLT\nr5coWN7cAuuBicwcsEhaNgQv\n-----END PRIVATE KEY-----\n"; 
 const SPREADSHEET_ID = '1tFuSlM72F9J1A5oBMzLzguMq9tvSVy2CMc_NtWzItLA';
@@ -77,8 +79,7 @@ app.post('/', async (req, res) => {
 app.get('/', async (req, res) => {
   try {
     const data = await getDataFromGoogleSheet();
-    res.json(data); // Or render an HTML page with this data
-    res.json({ message: 'CORS is working!' });
+    res.render('index', { data });
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Error fetching data from Google Sheets' });
