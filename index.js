5afc5ad20f8f6ada960319dfe22a4bdd0efe978a
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-09-17 14:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowedOrigins = ['https://formpayment.ogcs.co.in'];
+const allowedOrigins = ['https://formpayment.ogcs.co.in','https://form.ogcs.co.in/register.html'];
 
 app.use(cors({
   origin: allowedOrigins,
